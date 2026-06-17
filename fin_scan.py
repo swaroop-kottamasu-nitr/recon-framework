86e3d944c592332c811702b88fb970980911d331
swaroop-kottamasu-nitr <kjs92611@gmail.com>
2026-06-18 01:52:41 +0530

    Improved FIN scan output formatting

---
+++ fin_scan.py
@@ -0,0 +1,50 @@
+from concurrent.futures import ThreadPoolExecutor
+from scapy.all import *
+
+target = input("Enter target IP: ")
+cc=0
+oc=[]
+start_port = int(input("Start port: "))
+end_port = int(input("End port: "))
+
+print(f"\nFIN Scanning {target}...\n")
+
+
+def fin_scan(port):
+
+    packet = IP(dst=target) / TCP(
+        dport=port,
+        flags="F"
+    )
+
+    response = sr1(
+        packet,
+        timeout=1,
+        verbose=0
+    )
+
+    if response:
+
+        if response.haslayer(TCP):
+
+            if response[TCP].flags & 0x04:
+                
+                cc+=1
+
+    else:
+        
+        oc.append(port)
+
+
+ports = range(start_port, end_port + 1)
+
+with ThreadPoolExecutor(max_workers=50) as executor:
+    executor.map(fin_scan, ports)
+print("\nScan Complete!")
+print("\nFIN Scan Results:")
+print("------------------")
+print(f"\nTotal Closed Ports: {cc}")
+if oc:
+    print("\nOpen|Filtered Ports:\n")
+    print(oc)
+print(f"\nTotal Open|Filtered Ports: {len(oc)}")
